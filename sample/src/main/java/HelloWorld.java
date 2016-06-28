545e47e3cbb1410d786535aa613b2bfd518aa9d1
Francois Fouquet <fouquet.f@gmail.com>
2016-06-28 12:00:40 +0200

    modify helloworld pass to v4 of mwg

--- sample/src/main/java/HelloWorld.java
+++ sample/src/main/java/HelloWorld.java
@@ -1,6 +1,5 @@
 import cloud.Server;
-import org.mwg.Callback;
-import org.mwg.Graph;
+
 import org.mwg.GraphBuilder;
 import cloud.Cloud;
 
@@ -8,23 +7,20 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        sampleModel model = new sampleModel(new GraphBuilder());
-        model.graph().connect(new Callback<Boolean>() {
-            @Override
-            public void on(Boolean result) {
-                Cloud cloud = model.newCloud(0, 0);
-                Server server = model.newServer(0, 0);
-                server.setName("Hello");
-                System.out.println(server.getName());
-                System.out.println(server);
+        sampleModel model = new sampleModel(new GraphBuilder().withOffHeapMemory());
+        model.graph().connect(result -> {
+            Cloud cloud = model.newCloud(0, 0);
+            Server server = model.newServer(0, 0);
+            server.setName("Hello");
+            System.out.println(server.getName());
+            System.out.println(server);
 
-                cloud.addToServers(server);
-                System.out.println(cloud);
+            cloud.addToServers(server);
+            System.out.println(cloud);
 
-                Server[] servers = cloud.getServers();
-                System.out.println(servers);
-                System.out.println(servers[0]);
-            }
+            Server[] servers = cloud.getServers();
+            System.out.println(servers);
+            System.out.println(servers[0]);
         });
 
 
