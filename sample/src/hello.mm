c7cf38372bf7497fc3785e60fbf611e04da7968d
Francois Fouquet <fouquet.f@gmail.com>
2016-06-30 08:10:22 +0200

    new version

--- sample/src/hello.mm
+++ sample/src/hello.mm
@@ -2,29 +2,25 @@ enum hello.SmartMeterState {
     InError, Healthy, Pending
 }
 
-index clouds : cloud.Software {
+index clouds: cloud.Software {
     name
 }
 
 class cloud.Cloud {
-    rel servers : cloud.Server
+    rel servers: cloud.Server
 }
+
 class cloud.Server {
-    att name : String
-    rel softwares : cloud.Software
-
-    index softwares2 : cloud.Software {
-
-    }
-
+    att name: String
+    rel softwares: cloud.Software
 }
-class cloud.Software {
-    att name : String
-    derived att load : Double {
-        using "PolynomialNode"
-        with precision 0.1
-    }
-    att loadProfile : Double {
 
+class cloud.Software {
+    att name: String
+    att load: Double {
+        using "PolynomialNode"
+        with precision "0.1"
+    }
+    att loadProfile: Double {
     }
 }
